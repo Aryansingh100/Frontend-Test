568282a05daa006b9ed2d552a08f80cff92abc98
Aryan <sin14784@sheridancollege.ca>
2026-03-20 00:03:32 -0400

    Added the Priority, Filter, and the addTask function

--- modules/test.tsx
+++ modules/test.tsx
@@ -1,11 +1,32 @@
 'use client'
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from './test.module.css';
 
 // Your Test Starts Here
 
-export default function TaskManager(): JSX.Element {
-    return <div className={styles.container}>
-        
-    </div>;
-};
+// Priority control
+type Priority = "Low" | "Medium" | "High";
+
+// Filter control
+type Filter = "All" | "Active" | "Completed";
+
+interface Task {
+    id : string;
+    title : string;
+    priority : Priority;
+    finished : boolean;
+}
+
+export default function TaskManager():  {
+    const [tasks, setTasks] = useState]>([]);
+    const [inputTitle, setInputTitle] = useState("");
+    const [priority, setPriority] = useState<Priority>("Medium");
+    const [error, setError] = useState("");
+
+    function addTask(){
+        if (!inputTitle.trim()){
+            setError("Title cannot be empty");
+            return;
+        }
+    }
+}
